ae8c0b7e8f4eb24b80af926f59fe14337ef80305
1000ch <shogosensui@gmail.com>
2021-11-21 13:04:44 +0900

    feat: add tagName declaration

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,10 @@ import FlexContainer from './flex-container.js';
 import FlexItem from './flex-item.js';
 
 export { FlexContainer, FlexItem };
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'flex-container': FlexContainer;
+    'flex-item': FlexItem;
+  }
+}
