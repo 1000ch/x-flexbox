5eb4420508cfdaea344b1b5c75a02bfca8aaefb5
1000ch <shogosensui@gmail.com>
2021-11-22 17:48:40 +0900

    test: add tests

--- test/flex-container.test.ts
+++ test/flex-container.test.ts
@@ -1,7 +1,15 @@
 import FlexContainer from '../src/flex-container';
 
 describe('FlexContainer', (): void => {
+  beforeAll(() => {
+    customElements.define('flex-container', FlexContainer);
+  });
+
   test('should be defined', (): void => {
     expect(FlexContainer).toBeDefined();
+    expect(customElements.get('flex-container')).toBe(FlexContainer);
+
+    const container = new FlexContainer();
+    expect(container).toBeInstanceOf(HTMLElement);
   });
 })
--- test/flex-item.test.ts
+++ test/flex-item.test.ts
@@ -1,7 +1,15 @@
 import FlexItem from '../src/flex-item';
 
 describe('FlexItem', (): void => {
+  beforeAll(() => {
+    customElements.define('flex-item', FlexItem);
+  });
+
   test('should be defined', (): void => {
     expect(FlexItem).toBeDefined();
+    expect(customElements.get('flex-item')).toBe(FlexItem);
+
+    const item = new FlexItem();
+    expect(item).toBeInstanceOf(HTMLElement);
   });
 })
